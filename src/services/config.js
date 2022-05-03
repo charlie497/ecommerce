2428489b57c57e6a528501f345960feff81d48bd
hemcharan <hemcharan54@gmail.com>
2022-05-03 16:53:41 +0530

    ecommerce

---
+++ src/services/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD-DtZYBOmkrhsiUhhgzW7_ZOdlBL7icq0",
+  authDomain: "peppy-castle-307907.firebaseapp.com",
+  databaseURL: "https://peppy-castle-307907-default-rtdb.firebaseio.com",
+  projectId: "peppy-castle-307907",
+  storageBucket: "peppy-castle-307907.appspot.com",
+  messagingSenderId: "295760600830",
+  appId: "1:295760600830:web:3b3c2ac425713d612fcfaf",
+  measurementId: "G-ZBPD77EVH2"
+};
+
+export default firebaseConfig;
